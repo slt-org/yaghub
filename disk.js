fce66a39ff04ac4d0c89909fa68ea37358c845b6
sterry1 <sterry1@protonmail.com>
2022-05-09 14:48:22 -0500

    Restructure code to allow for multiple calculators each having a different page.  * disk  * cuboid  * anyVolume
    doesn't seem to change the functionality (fingers crossed)

---
+++ disk.js
@@ -0,0 +1,10 @@
+/**
+ * This function will calculate a disk volume based on
+ * centimeter inputs, generating a volume in cubic centimeters.
+ *  
+ */
+function diskCalcMetric(radius,height){
+  console.log("value parameters on call radius="+radius+" height="+height);
+  var volume = new Object();
+
+}
